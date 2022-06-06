2af2317bbe52469b7327b63810ffb739fc20de3c
dhanyaal <dh11any@hotmail.com>
2022-06-06 15:16:30 +0100

    Car DotNet MVC Web App

---
+++ SimpleDotNetCoreMVCApp/SimpleDotNetCoreMVCApp/Controllers/CarController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleDotNetCoreMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleDotNetCoreMVCApp.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new CarsModel();
+            model.SelectCarList.Add(new CarModelListItem()
+            {
+                Id = 3,
+                Name = "Bugatti",
+                ModelName = "Chiron",
+                Price = 10000000
+            });
+            return View(model);
+        }
+    }
+}
---
+++ SimpleDotNetCoreMVCApp/SimpleDotNetCoreMVCApp/Models/CarModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleDotNetCoreMVCApp.Models
+{
+    public class CarModelListItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ModelName { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CarsModel
+    {
+       private List<CarModelListItem> carList = null;
+
+        public CarsModel()
+        {
+            carList = new List<CarModelListItem>() {
+                new CarModelListItem() {
+                    Id = 1,
+                    Name = "BMW",
+                    ModelName = "BMW S1",
+                    Price = 125000
+                },
+               new CarModelListItem() {
+                    Id = 2,
+                    Name = "Mercedes",
+                    ModelName = "AMG Sport",
+                    Price = 20000
+                }
+            };
+        }
+
+        public List<CarModelListItem> SelectCarList
+        {
+            get {
+                return carList;
+            }
+
+            set
+            {
+                carList = value;
+            }
+        }
+    }
+}
